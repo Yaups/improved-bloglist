a5a5b40f7e8c8a3b139c9ca59e843e3e73876932
Yaups <tpallison.ta@gmail.com>
2024-04-04 16:30:32 +0100

    Hopefully fixed ci-cd pipeline

--- server/controllers/diagnosticsRouter.js
+++ server/controllers/diagnosticsRouter.js
@@ -1,7 +1,7 @@
 const diagnosticsRouter = require('express').Router()
 
 diagnosticsRouter.get('/version', (_req, res) => {
-  res.send('loop')
+  res.send('sausage2')
 })
 
 diagnosticsRouter.get('/health', (_req, res) => {
