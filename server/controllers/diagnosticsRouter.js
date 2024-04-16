46d515d5c52c668ad9ed6ff26077ef60cb29ef7b
Yaups <tpallison.ta@gmail.com>
2024-04-16 14:44:27 +0100

    Final touches for production portfolio

--- server/controllers/diagnosticsRouter.js
+++ server/controllers/diagnosticsRouter.js
@@ -1,7 +1,7 @@
 const diagnosticsRouter = require('express').Router()
 
 diagnosticsRouter.get('/version', (_req, res) => {
-  res.send('verdant')
+  res.send('yarg')
 })
 
 diagnosticsRouter.get('/health', (_req, res) => {
